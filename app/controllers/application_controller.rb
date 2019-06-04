6de846fce356c83dd3d704789afe40b7d614a158
wai-doi <yusuke.doi@esm.co.jp>
2019-06-04 17:22:13 +0900

    管理機能を管理者ユーザーだけに利用させるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def login_required
-    redirct_to login_url unless current_user
+    redirect_to login_url unless current_user
   end
 end
